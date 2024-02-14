86b05f882c577ca4f4adc0108201414324e2aceb
Abena Boatemaa Asare-Ansah <110104000+MsPixels@users.noreply.github.com>
2024-02-14 11:37:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'The Title Text of this Story',
+    title: 'Where Do Refugees in Uganda Come From?',
     subtitle: 'A descriptive and interesting subtitle to draw in the reader',
     byline: 'By a Digital Storyteller',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
